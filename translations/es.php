090393c7587caa68b2411b0707f0253892e96dce
tortextrx <39431480+tortextrx@users.noreply.github.com>
2018-06-19 14:16:47 +0200

    create es.php
    it's just a translation into Spanish

---
+++ translations/es.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{amp}prestashop>amp_9d12d0559ba95df7f577f0a385fa59a6'] = 'Este módulo añade una pagina amp a las categorias y a los productos';
+$_MODULE['<{amp}prestashop>amp_9692295adc4aea7c167bdc2547a23104'] = 'Este módulo debe tener la opción Desplazar JavaScript al final del documento HTML deshabilitado en Configuración avanzada / Menú de optimización del backoffice';
+$_MODULE['<{amp}prestashop>category_97e0d62d1db5473366dd6e7ebd56f596'] = 'Logo de la tienda';
+$_MODULE['<{amp}prestashop>category_ec9377b52a060cbb273770451b1c5bfe'] = 'Mostrar';
+$_MODULE['<{amp}prestashop>category_7e7e076a81fa5dc64f44b8ff14199cbf'] = 'De';
+$_MODULE['<{amp}prestashop>category_9827bba91308f20e6080632f44cd8509'] = 'Items.';
+$_MODULE['<{amp}prestashop>category_dd1f775e443ff3b9a89270713580a51b'] = 'Anterior';
+$_MODULE['<{amp}prestashop>category_10ac3d04253ef7e1ddc73e6091c0cd55'] = 'Siguiente';
+$_MODULE['<{amp}prestashop>category_2d0f6b8300be19cf35e89e66f0677f95'] = 'Añadir al carrito';
+$_MODULE['<{amp}prestashop>category_17_97e0d62d1db5473366dd6e7ebd56f596'] = 'Logo de la tienda';
+$_MODULE['<{amp}prestashop>category_17_2d0f6b8300be19cf35e89e66f0677f95'] = 'Añadir al carrito';
+$_MODULE['<{amp}prestashop>product_97e0d62d1db5473366dd6e7ebd56f596'] = 'Logo de la tienda';
+$_MODULE['<{amp}prestashop>product_63d5049791d9d79d86e9a108b0a999ca'] = 'Referencia';
+$_MODULE['<{amp}prestashop>product_2d0f6b8300be19cf35e89e66f0677f95'] = 'Añadir al carrito';
+$_MODULE['<{amp}prestashop>product_17_97e0d62d1db5473366dd6e7ebd56f596'] = 'Shop logo';
+$_MODULE['<{amp}prestashop>product_17_63d5049791d9d79d86e9a108b0a999ca'] = 'Referencia';
+$_MODULE['<{amp}prestashop>product_17_2d0f6b8300be19cf35e89e66f0677f95'] = 'Añadir al carrito';
